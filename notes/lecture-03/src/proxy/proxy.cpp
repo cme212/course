2c582570fe528da045ad9b8c8b2b50c73377d0d3
Geronimo <a@b.com>
2018-01-16 21:06:51 -0800

    Added simple example for proxy pattern.

---
+++ notes/lecture-03/src/proxy/proxy.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "student.hpp"
+
+class StudentProxy
+{
+public:
+  StudentProxy()
+  {
+  }
+
+  ~StudentProxy()
+  {
+    if(student_ != nullptr)
+      delete student_;
+  }
+
+  void create(std::string name, int studentID, long creditCardNo)
+  {
+    student_ = new Student(name, studentID, creditCardNo);
+  }
+
+  std::string getName()
+  {
+    return student_->getName();
+  }
+
+private:
+  Student* student_;
+};
+
+int main()
+{
+  // Create vector of proxy Students
+  std::vector<StudentProxy> ss(3);
+
+  // Create real Students
+  ss[0].create("Terry Gilliam", 123444, 1111222233334);
+  ss[1].create("Eric Idle"    , 123555, 5555222233334);
+  ss[2].create("Michael Palin", 123666, 6666222233334);
+
+  // Do something with Students
+  for (unsigned i=0; i<ss.size(); ++i)
+    std::cout << ss[i].getName() << "\n";
+
+  return 0;
+}
