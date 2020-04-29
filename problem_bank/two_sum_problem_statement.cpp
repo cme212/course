9afe764c0b841336515cad77218a27c04200bb13
Andreas Santucci <santucci.andreas@gmail.com>
2020-04-28 20:21:37 -0700

    Problem statement.

---
+++ problem_bank/two_sum_problem_statement.cpp
@@ -0,0 +1,5 @@
+/* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+vector<int> twoSum(vector<int>& nums, int target);
