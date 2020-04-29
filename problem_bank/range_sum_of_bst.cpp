ca4542485e55787240126044aef53f48f54f0fb1
Andreas Santucci <santucci.andreas@gmail.com>
2020-04-28 20:55:34 -0700

    Problem statement + Examples.

---
+++ problem_bank/range_sum_of_bst.cpp
@@ -0,0 +1,23 @@
+/* Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+   The binary search tree is guaranteed to have unique values.
+
+   Example 1:
+   Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+   Output: 32
+   
+   Example 2:
+   Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+   Output: 23
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int rangeSumBST(TreeNode* root, int L, int R);
