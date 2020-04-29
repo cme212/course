a72d2a734758f06a80ecf86a6704372f4617902b
Andreas Santucci <santucci.andreas@gmail.com>
2020-04-28 20:51:11 -0700

    Problem statement + examples.

---
+++ problem_bank/split_a_string_in_balanced_strings.cpp
@@ -0,0 +1,30 @@
+/* Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+   Given a balanced string s split it in the maximum amount of balanced strings.
+   Return the maximum amount of splitted balanced strings.
+
+   Example 1:
+   Input: s = "RLRRLLRLRL"
+   Output: 4
+   Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+   Example 2:   
+   Input: s = "RLLLLRRRLR"
+   Output: 3
+   Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+   Example 3: 
+   Input: s = "LLLLRRRR"
+   Output: 1
+   Explanation: s can be split into "LLLLRRRR".
+
+   Example 4:   
+   Input: s = "RLRRRLLRLL"
+   Output: 2
+   Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+*/
+int balancedStringSplit(string s);
+
+
+
+
+
