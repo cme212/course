9127a50639476e0e94877ef9b69cdb0953f789df
Andreas Santucci <santucci.andreas@gmail.com>
2020-04-28 20:35:11 -0700

    Problem statement.

---
+++ problem_bank/intersection_three_sorted_arrays.cpp
@@ -0,0 +1,5 @@
+/* Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, 
+   return a sorted array of only the integers that appeared in all three arrays.
+*/
+vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3);
+
